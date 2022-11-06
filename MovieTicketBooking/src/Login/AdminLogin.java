95e2b4c4632ccf2ecb11743c92c4ba3933bab220
Rishabh9933 <102218653+Rishabh9933@users.noreply.github.com>
2022-11-06 23:15:48 +0530

    Admin update (#1)
    * backup
    
    * backup
    
    * backup

--- MovieTicketBooking/src/Login/AdminLogin.java
+++ MovieTicketBooking/src/Login/AdminLogin.java
@@ -1,20 +1,63 @@
 package Login;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class AdminLogin implements LoginInterface{
-    @Override
-    public void inputPrompt(){
 
+    private String adminname;
+    private String password;
+
+    private boolean access;
+
+    public boolean getAccess() {
+        return access;
+    }
+    private void setAccess(boolean access) {this.access = access;}
+
+    public AdminLogin(){
+        setAdminname("");
+        setPassword("");
+        access = false;
+    }
+    private String getAdminname() {
+        return adminname;
     }
 
+    private void setAdminname(String adminname) {
+        this.adminname = adminname;
+    }
+    private String getPassword() {
+        return password;
+    }
+    private void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public void inputPrompt(){
+        System.out.print("Enter Admin name: ");
+        Scanner scanner = new Scanner(System.in);
+        setAdminname(scanner.next());
 
-    public void connect() {
+        System.out.print("Enter password: ");
 
+        setPassword(scanner.next());
+        authentication();
     }
 
     @Override
     public void authentication() {
-
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Project", "admin", "Project@112");
+            String query = "select count(*) from Admin where adminname = ? and password = ?;";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setString(1, getAdminname());
+            pst.setString(2, getPassword());
+            ResultSet rs = pst.executeQuery();
+            rs.next();
+            setAccess(rs.getString(1).equals("1"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
