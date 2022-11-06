f5d352e7cfa70ca36fe7939fda36177babfc4ec9
Rishabh9933 <rishabhjha9933@gmail.com>
2022-11-07 04:07:06 +0530

    backup

--- MovieTicketBooking/src/Login/AdminLogin.java
+++ MovieTicketBooking/src/Login/AdminLogin.java
@@ -1,1 +1,15 @@
 package Login;
+
+import java.sql.*;
+
+public class AdminLogin implements LoginInterface{
+    @Override
+    public void inputPrompt(){
+
+    }
+
+    @Override
+    public void authentication() {
+
+    }
+}
