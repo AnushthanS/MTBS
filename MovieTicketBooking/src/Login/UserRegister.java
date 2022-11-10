291a8733a862124e51cbf6123922e6d245a5d2b3
Rishabh9933 <102218653+Rishabh9933@users.noreply.github.com>
2022-11-10 14:46:47 +0530

    backup (#4)

--- MovieTicketBooking/src/Login/UserRegister.java
+++ MovieTicketBooking/src/Login/UserRegister.java
@@ -1,4 +1,85 @@
 package Login;
 
-public class UserRegister {
+import javax.swing.*;
+import java.sql.*;
+import java.util.Scanner;
+
+public class UserRegister implements RegisterInterface{
+
+    private String username;
+    private String password;
+    private String name;
+    private String phoneNo;
+    public String getUsername() {
+        return username;
+    }
+    private void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    private void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    @Override
+    public void inputPrompt(){
+        System.out.print("Enter your Name: ");
+        Scanner scanner = new Scanner(System.in);
+        setName(scanner.next());
+
+        System.out.print("Enter username/email: ");
+        setUsername(scanner.next());
+
+        System.out.print("Enter password: ");
+        setPassword(scanner.next());
+
+        System.out.print("Enter phoneNo: ");
+        setPhoneNo(scanner.next());
+    }
+
+    @Override
+    public void dataEntry(){
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Project", "admin", "Project@112");
+            String query = "insert into User values(?,?,?,?)";
+            PreparedStatement pst = con.prepareStatement(query);
+
+            pst.setString(1, getName());
+            pst.setString(2, getUsername());
+            pst.setString(3, getPassword());
+            pst.setString(4, getPhoneNo());
+
+            if(pst.executeUpdate() > 0){
+                System.out.println("New User Added");
+            }
+//            ResultSet rs = pst.executeQuery();
+//            rs.next();
+           // setAccess(rs.getString(1).equals("1"));
+        }catch (SQLIntegrityConstraintViolationException e){
+            System.out.println("Username already registered");
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
