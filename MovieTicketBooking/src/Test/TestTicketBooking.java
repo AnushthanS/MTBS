7a02a7920da29329b8ec8f26a2f89546d45692f4
Anushthan Saxena <anushthans@gmail.com>
2022-11-14 17:08:40 +0530

    backup

---
+++ MovieTicketBooking/src/Test/TestTicketBooking.java
@@ -0,0 +1,13 @@
+package Test;
+
+import TicketBooking.TicketBooking;
+
+public class TestTicketBooking {
+    public static void main(String[] args) {
+        TicketBooking booking = new TicketBooking();
+        booking.AddDetails();
+
+
+
+    }
+}
